1c0f43bb100103175e5f9da9ed5cfdd2354db646
Adam Crabtree <adcrabtr@microsoft.com>
2020-02-26 11:45:32 -0800

    Release Verson 0.0.0.1

--- iOS/GraphNotificationsSample/NotificationsViewController.h
+++
@@ -1,9 +0,0 @@
-//  Copyright (c) Microsoft. Licensed under the MIT license.
-
-#pragma once
-
-#import <UIKit/UIKit.h>
-
-@interface NotificationsViewController : UITableViewController<UITableViewDelegate, UITableViewDataSource>
-- (IBAction)refresh;
-@end
